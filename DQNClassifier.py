600b3962b2a7bcbc6f18da7b8fe374b3a4b5d9cc
Sophie Rumeau <rumeau.sophie02@gmail.com>
2025-12-17 16:14:08 -0500

    ajout dropout

--- DQNClassifier.py
+++ DQNClassifier.py
@@ -2,13 +2,15 @@ import torch
 import torch.nn as nn
 
 class DQNClassifier(nn.Module):
-    def __init__(self, input_dim, n_classes, hidden_dim=128):
+    def __init__(self, input_dim, n_classes, hidden_dim=128, dropout_p=0.5):
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(input_dim, hidden_dim),
             nn.ReLU(),
+            nn.Dropout(dropout_p),
             nn.Linear(hidden_dim, hidden_dim),
             nn.ReLU(),
+            nn.Dropout(dropout_p),
             nn.Linear(hidden_dim, n_classes)
         )
 
